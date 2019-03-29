6c7fb4622df330afc483fc32012d6a21975176ca
Karakó Miklós <gh2@mail.palacsint.hu>
2019-03-29 10:48:26 +0100

    Adds tests projects for PureJavaComm
    Adds tests projects for PureJavaComm with different JNA dependencies
    to make testing https://github.com/nyholku/purejavacomm/issues/124
    easier.

---
+++ purejavacomm-tester-jna5/src/main/java/hu/palacsint/purejavacomm/purejavacomm_jna5/Main.java
@@ -0,0 +1,17 @@
+package hu.palacsint.purejavacomm.purejavacomm_jna5;
+
+import java.util.Enumeration;
+
+import purejavacomm.CommPortIdentifier;
+
+public final class Main {
+
+	public static void main(final String[] args) {
+		final Enumeration<CommPortIdentifier> portIdentifiers = purejavacomm.CommPortIdentifier.getPortIdentifiers();
+		while (portIdentifiers.hasMoreElements()) {
+			final CommPortIdentifier commPortIdentifier = portIdentifiers.nextElement();
+			System.out.println(commPortIdentifier.getName());
+		}
+		System.out.println("end");
+	}
+}
